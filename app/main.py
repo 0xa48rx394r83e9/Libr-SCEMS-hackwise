e07952822058500ae762606c26bf3c4c89228bf7
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2021-12-03 22:48:01 +0800

    It works

--- app/main.py
+++ app/main.py
@@ -1,21 +1,14 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from . import db
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
-socketio = SocketIO(app)
+main = Blueprint('main', __name__)
 
-@app.route('/')
-def sessions():
-    return render_template('session.html')
+@main.route('/')
+def index():
+    return render_template('index.html')
 
-def messageReceived(methods=['GET', 'POST']):
-    print('message was received!!!')
-
-@socketio.on('my event')
-def handle_my_custom_event(json, methods=['GET', 'POST']):
-    print('received my event: ' + str(json))
-    socketio.emit('my response', json, callback=messageReceived)
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
+@main.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', name=current_user.name)
