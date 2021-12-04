354791706450d632c11f7f616bc16fae92fbaf79
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2021-12-04 19:52:50 +0800

    merging process step two

--- app/auth.py
+++ app/auth.py
@@ -8,7 +8,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    return render_template('login1.html')
 
 @auth.route('/login', methods=['POST'])
 def login_post():
@@ -28,7 +28,7 @@ def login_post():
 
 @auth.route('/signup')
 def signup():
-    return render_template('signup.html')
+    return render_template('signup1.html')
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
