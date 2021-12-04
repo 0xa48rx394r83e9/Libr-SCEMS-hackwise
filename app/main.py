daa62ff0c0e9923b2e065ed361eb4102171f18b6
0xa48rx394r83e9 <Joseph.paulmer24@stu.dulwich.org>
2021-12-04 17:09:17 +0800

    merging process step one

--- app/main.py
+++ app/main.py
@@ -15,3 +15,38 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+@main.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.name)
+
+@main.route('/classroom')
+@login_required
+def classroom():
+    return render_template('classroom.html', name=current_user.name)
+
+@main.route('/courses')
+@login_required
+def courses():
+    return render_template('courses.html', name=current_user.name)
+
+@main.route('/past_papers')
+@login_required
+def past_papers():
+    return render_template('file-dir.html', name=current_user.name)
+
+@main.route('/projects')
+@login_required
+def projects():
+    return render_template('projects.html', name=current_user.name)
+
+@main.route('/universities') #WIP
+@login_required
+def universities():
+    return render_template('uni.html', name=current_user.name)
+
+# @main.route('/ide') #WIP
+# @login_required
+# def ide():
+#     return render_template('profile.html', name=current_user.name)
